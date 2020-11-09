47fccfebb298cc7ced7e7fd49008cb82acba4e1d
Futo Yoshizuka <hw17a211@oecu.jp>
2020-11-09 10:17:45 +0900

    TitleからSong、Tutorialへ進めるように更新

---
+++ Perform of Music/New Unity Project/Assets/Scripts/TitleSceneManager_Start.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager_Start : MonoBehaviour {
+
+	public void TAP () {
+		if (Input.GetMouseButtonDown(0)) { //Startがタッチされたら
+			SceneManager.LoadScene("Song"); //songへ
+		}
+	}
+}
---
+++ Perform of Music/New Unity Project/Assets/Scripts/TitleSceneManager_Tutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager_Tutorial : MonoBehaviour {
+
+	public void TAP () {
+		if (Input.GetMouseButtonDown(0)) { //Tutorialがタッチされたら
+			SceneManager.LoadScene("Tutorial"); //Tutorialへ
+		}
+	}
+}
